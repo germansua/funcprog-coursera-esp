a2aa94f00939a8313672700f2f2665505d7456d6
German Alonso Suarez Guerrero <gersua@GERMANs-MacBook-Pro.local>
2017-03-19 11:41:36 -0500

    Week 4

--- FuncProg2/exercises/week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ FuncProg2/exercises/week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,24 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+    Var(scala.math.pow(b(), 2) - (4 * a() * c()))
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+
+    val minusB = Var(-1 * b())
+    val deltaSqrt = Var(scala.math.sqrt(delta()))
+    val divisor = Var(2 * a())
+
+    Var(
+      if (delta() < 0) Set()
+      else {
+        val positiveSol = Var((minusB() + deltaSqrt()) / divisor())
+        val negativeSol = Var((minusB() - deltaSqrt()) / divisor())
+        Set(positiveSol(), negativeSol())
+      }
+    )
   }
 }
