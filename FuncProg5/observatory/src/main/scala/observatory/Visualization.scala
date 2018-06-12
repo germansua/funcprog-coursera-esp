df7cbb2ca84bc2525b1c244460f75990af9bacda
German Alonso Suárez Guerrero <gersua@Germans-MacBook-Pro.local>
2018-06-12 09:55:05 -0500

    Calculate IDW

--- FuncProg5/observatory/src/main/scala/observatory/Visualization.scala
+++ FuncProg5/observatory/src/main/scala/observatory/Visualization.scala
@@ -18,17 +18,55 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    // If the location is into the temperatures, returns that value
+    // Else, apply inverse distance weighting and approximate the temperature value
+    temperatures
+      .find(_._1 == location)
+      .map(_._2)
+      .getOrElse(calculatePrediction(temperatures, location))
   }
 
-  def calculateCentralAngle(location1: Location, location2: Location): Double = {
+  private def calculatePrediction(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
+    // Calculate the distance for each location
+    // If there's a location with less than 1km, return that value as the prediction
+    // Otherwise, calculate the described formula for all points
+    calculateInverseDistanceWeighting {
+      temperatures
+        .map(temp => (calculateDistanceBetweenLocations(temp._1, location), temp._2))
+    }
+
+    //      .find(iter => iter._1 < 1000)
+    //      .map(_._2)
+    //      .getOrElse(calculateInverseDistanceWeighting())
+  }
+
+  private def calculateInverseDistanceWeighting(values: Iterable[(Double, Temperature)]): Temperature = {
+    // zp: Value of predicted point
+    // zi: Value of i measured point
+    // d(i): Distance of i measured to the predicted point
+    // P: Power function
+    // zp = sum( zi / pow(di, P) ) / sum( 1 / pow(di, P) )
+
+    def calculateAccumulators(values: Iterable[(Double, Temperature)],
+                              upperAcc: Double, lowerAcc: Double): (Double, Double) = values match {
+      case x :: xs => calculateAccumulators(xs, upperAcc + (x._2 / pow(x._1, 2)), lowerAcc + 1 / pow(x._1, 2))
+      case Nil => (upperAcc, lowerAcc)
+    }
+
+    val result = calculateAccumulators(values, 0, 0)
+    result._1 / result._2
+  }
+
+  def calculateDistanceBetweenLocations(location1: Location, location2: Location): Double = {
+    calculateDistance(calculateCentralAngle(location1, location2))
+  }
+
+  private def calculateCentralAngle(location1: Location, location2: Location): Double = {
     val lat1 = toRadians(location1.lat)
     val lon1 = toRadians(location1.lon)
     val lat2 = toRadians(location2.lat)
     val lon2 = toRadians(location2.lon)
-
-    val centralAngle = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(abs(lon1 - lon2)))
-    calculateDistance(centralAngle)
+    acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(abs(lon1 - lon2)))
   }
 
   private def calculateDistance(centralAngle: Double): Double = meanEarthRadiusKMs * centralAngle
